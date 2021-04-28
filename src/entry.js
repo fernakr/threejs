afc677920f1b553fdee4ae3fa5f3342a55c4ac14
Kristine Fernandez <kfernandez@mightycitizen.com>
2021-04-28 09:41:51 -0500

    remove old commented out code

--- src/entry.js
+++ src/entry.js
@@ -8,8 +8,6 @@
  */
 
 //import * as THREE from 'three';
-//import CANNON from 'cannon';
-//import { Ammo } from 'three-ammo';
 import { THREE } from 'enable3d'
 import { AmmoPhysics, ExtendedMesh, PhysicsLoader } from '@enable3d/ammo-physics'
 
@@ -23,272 +21,6 @@ import treatModel from './assets/models/treat/treat.fbx';
 
 
 const MainScene = () => {
-  //const groundMat = new CANNON.Material();
-  // const pugMat = new CANNON.Material();
-  // const objectMat = new CANNON.Material();
-
-  // const contactMaterial = new CANNON.ContactMaterial(groundMat, objectMat, {
-  //     friction: 0.3,
-  //     restitution: 0.1
-  // });
-
-
-
-  // const contactMaterial2 = new CANNON.ContactMaterial(objectMat, objectMat, {
-  //   friction: 1,
-  //   restitution: 0.1
-  // });
-
-  // const contactMaterial3 = new CANNON.ContactMaterial(groundMat, pugMat, {
-  //   friction: 0.2,
-  //   restitution: 0,
-  //   contactEquationStiffness: 1e12,
-  //   contactEquationRelaxation: 3,
-  //   frictionEquationStiffness: 1e8,
-  //   frictionEquationRegularizationTime: 0.1,
-  //   contactEquationRegularizationTime: 0.2,
-
-  // });
-
-
-  // const contactMaterial4 = new CANNON.ContactMaterial(objectMat, pugMat, {
-  //   friction: 10,
-  //   restitution: 0.2
-  // });
-
-
-
-
-  // THREE.CannonDebugRenderer = function(scene, world, options){
-  //   options = options || {};
-
-  //   this.scene = scene;
-  //   this.world = world;
-
-  //   this._meshes = [];
-
-  //   this._material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-  //   this._sphereGeometry = new THREE.SphereGeometry(1);
-  //   this._boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-  //   this._planeGeometry = new THREE.PlaneGeometry( 10, 10, 10, 10 );
-  //   this._cylinderGeometry = new THREE.CylinderGeometry( 1, 1, 10, 10 );
-  // };
-
-  // THREE.CannonDebugRenderer.prototype = {
-
-  //   tmpVec0: new CANNON.Vec3(),
-  //   tmpVec1: new CANNON.Vec3(),
-  //   tmpVec2: new CANNON.Vec3(),
-  //   tmpQuat0: new CANNON.Vec3(),
-
-  //   update: function(){
-
-  //       var bodies = this.world.bodies;
-  //       var meshes = this._meshes;
-  //       var shapeWorldPosition = this.tmpVec0;
-  //       var shapeWorldQuaternion = this.tmpQuat0;
-
-  //       var meshIndex = 0;
-
-  //       for (var i = 0; i !== bodies.length; i++) {
-  //           var body = bodies[i];
-
-  //           for (var j = 0; j !== body.shapes.length; j++) {
-  //               var shape = body.shapes[j];
-
-  //               this._updateMesh(meshIndex, body, shape);
-
-  //               var mesh = meshes[meshIndex];
-
-  //               if(mesh){
-
-  //                   // Get world position
-  //                   body.quaternion.vmult(body.shapeOffsets[j], shapeWorldPosition);
-  //                   body.position.vadd(shapeWorldPosition, shapeWorldPosition);
-
-  //                   // Get world quaternion
-  //                   body.quaternion.mult(body.shapeOrientations[j], shapeWorldQuaternion);
-
-  //                   // Copy to meshes
-  //                   mesh.position.copy(shapeWorldPosition);
-  //                   mesh.quaternion.copy(shapeWorldQuaternion);
-  //               }
-
-  //               meshIndex++;
-  //           }
-  //       }
-
-  //       for(var i = meshIndex; i < meshes.length; i++){
-  //           var mesh = meshes[i];
-  //           if(mesh){
-  //               this.scene.remove(mesh);
-  //           }
-  //       }
-
-  //       meshes.length = meshIndex;
-  //   },
-
-  //   _updateMesh: function(index, body, shape){
-  //       var mesh = this._meshes[index];
-  //       if(!this._typeMatch(mesh, shape)){
-  //           if(mesh){
-  //               this.scene.remove(mesh);
-  //           }
-  //           mesh = this._meshes[index] = this._createMesh(shape);
-  //       }
-  //       this._scaleMesh(mesh, shape);
-  //   },
-
-  //   _typeMatch: function(mesh, shape){
-  //       if(!mesh){
-  //           return false;
-  //       }
-  //       var geo = mesh.geometry;
-  //       return (
-  //           (geo instanceof THREE.SphereGeometry && shape instanceof CANNON.Sphere) ||
-  //           (geo instanceof THREE.BoxGeometry && shape instanceof CANNON.Box) ||
-  //           (geo instanceof THREE.PlaneGeometry && shape instanceof CANNON.Plane) ||
-  //           (geo.id === shape.geometryId && shape instanceof CANNON.ConvexPolyhedron) ||
-  //           (geo.id === shape.geometryId && shape instanceof CANNON.Trimesh) ||
-  //           (geo.id === shape.geometryId && shape instanceof CANNON.Heightfield)
-  //       );
-  //   },
-
-  //   _createMesh: function(shape){
-  //       var mesh;
-  //       var material = this._material;
-
-  //       switch(shape.type){
-
-  //       case CANNON.Shape.types.SPHERE:
-  //           mesh = new THREE.Mesh(this._sphereGeometry, material);
-  //           break;
-
-  //       case CANNON.Shape.types.BOX:
-  //           mesh = new THREE.Mesh(this._boxGeometry, material);
-  //           break;
-
-  //       case CANNON.Shape.types.PLANE:
-  //           mesh = new THREE.Mesh(this._planeGeometry, material);
-  //           break;
-
-  //       case CANNON.Shape.types.CONVEXPOLYHEDRON:
-  //           // Create mesh
-  //           var geo = new THREE.Geometry();
-
-  //           // Add vertices
-  //           for (var i = 0; i < shape.vertices.length; i++) {
-  //               var v = shape.vertices[i];
-  //               geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
-  //           }
-
-  //           for(var i=0; i < shape.faces.length; i++){
-  //               var face = shape.faces[i];
-
-  //               // add triangles
-  //               var a = face[0];
-  //               for (var j = 1; j < face.length - 1; j++) {
-  //                   var b = face[j];
-  //                   var c = face[j + 1];
-  //                   geo.faces.push(new THREE.Face3(a, b, c));
-  //               }
-  //           }
-  //           geo.computeBoundingSphere();
-  //           geo.computeFaceNormals();
-
-  //           mesh = new THREE.Mesh(geo, material);
-  //           shape.geometryId = geo.id;
-  //           break;
-
-  //       case CANNON.Shape.types.TRIMESH:
-  //           var geometry = new THREE.Geometry();
-  //           var v0 = this.tmpVec0;
-  //           var v1 = this.tmpVec1;
-  //           var v2 = this.tmpVec2;
-  //           for (var i = 0; i < shape.indices.length / 3; i++) {
-  //               shape.getTriangleVertices(i, v0, v1, v2);
-  //               geometry.vertices.push(
-  //                   new THREE.Vector3(v0.x, v0.y, v0.z),
-  //                   new THREE.Vector3(v1.x, v1.y, v1.z),
-  //                   new THREE.Vector3(v2.x, v2.y, v2.z)
-  //               );
-  //               var j = geometry.vertices.length - 3;
-  //               geometry.faces.push(new THREE.Face3(j, j+1, j+2));
-  //           }
-  //           geometry.computeBoundingSphere();
-  //           geometry.computeFaceNormals();
-  //           mesh = new THREE.Mesh(geometry, material);
-  //           shape.geometryId = geometry.id;
-  //           break;
-
-  //       case CANNON.Shape.types.HEIGHTFIELD:
-  //           var geometry = new THREE.Geometry();
-
-  //           var v0 = this.tmpVec0;
-  //           var v1 = this.tmpVec1;
-  //           var v2 = this.tmpVec2;
-  //           for (var xi = 0; xi < shape.data.length - 1; xi++) {
-  //               for (var yi = 0; yi < shape.data[xi].length - 1; yi++) {
-  //                   for (var k = 0; k < 2; k++) {
-  //                       shape.getConvexTrianglePillar(xi, yi, k===0);
-  //                       v0.copy(shape.pillarConvex.vertices[0]);
-  //                       v1.copy(shape.pillarConvex.vertices[1]);
-  //                       v2.copy(shape.pillarConvex.vertices[2]);
-  //                       v0.vadd(shape.pillarOffset, v0);
-  //                       v1.vadd(shape.pillarOffset, v1);
-  //                       v2.vadd(shape.pillarOffset, v2);
-  //                       geometry.vertices.push(
-  //                           new THREE.Vector3(v0.x, v0.y, v0.z),
-  //                           new THREE.Vector3(v1.x, v1.y, v1.z),
-  //                           new THREE.Vector3(v2.x, v2.y, v2.z)
-  //                       );
-  //                       var i = geometry.vertices.length - 3;
-  //                       geometry.faces.push(new THREE.Face3(i, i+1, i+2));
-  //                   }
-  //               }
-  //           }
-  //           geometry.computeBoundingSphere();
-  //           geometry.computeFaceNormals();
-  //           mesh = new THREE.Mesh(geometry, material);
-  //           shape.geometryId = geometry.id;
-  //           break;
-  //       }
-
-  //       if(mesh){
-  //           this.scene.add(mesh);
-  //       }
-
-  //       return mesh;
-  //   },
-
-  //   _scaleMesh: function(mesh, shape){
-  //       switch(shape.type){
-
-  //       case CANNON.Shape.types.SPHERE:
-  //           var radius = shape.radius;
-  //           mesh.scale.set(radius, radius, radius);
-  //           break;
-
-  //       case CANNON.Shape.types.BOX:
-  //           mesh.scale.copy(shape.halfExtents);
-  //           mesh.scale.multiplyScalar(2);
-  //           break;
-
-  //       case CANNON.Shape.types.CONVEXPOLYHEDRON:
-  //           mesh.scale.set(1,1,1);
-  //           break;
-
-  //       case CANNON.Shape.types.TRIMESH:
-  //           mesh.scale.copy(shape.scale);
-  //           break;
-
-  //       case CANNON.Shape.types.HEIGHTFIELD:
-  //           mesh.scale.set(1,1,1);
-  //           break;
-
-  //       }
-  //   }
-  // };
 
   // init global vars
   let mixers = [];
@@ -379,7 +111,7 @@ const MainScene = () => {
     }
   ]
 
-  const setupInit = () => {
+  const init = () => {
 
     // renderer
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -566,14 +298,6 @@ const MainScene = () => {
       scene.add(mesh);
       physics.add.existing(mesh)
 
-      // const sphereShape = new CANNON.Box(new CANNON.Vec3(2, 2, 2))
-      // const sphereBody = new CANNON.Body({ mass: 0, material: objectMat});
-      // sphereBody.addShape(sphereShape)
-      // sphereBody.position.x = mesh.position.x
-      // sphereBody.position.y = mesh.position.y
-      // sphereBody.position.z = mesh.position.z
-      // world.addBody(sphereBody)
-
     }
   }
 
@@ -586,15 +310,35 @@ const MainScene = () => {
     plane.position.y = 0;
     scene.add(plane);
     scene.fog = new THREE.Fog(clearColor,60,100);
-    
+
     physics.add.ground({ width: 2000, height: 2000 })
   }
+  const createTriangleShapeByBufferGeometry = (geometry, scalingFactor) => {
+    var mesh = new Ammo.btTriangleMesh(true, true);
+    var vertexPositionArray = geometry.attributes.position.array;
+    for (var i = 0; i < geometry.attributes.position.count/3; i++) {
+            mesh.addTriangle(
+                new Ammo.btVector3(vertexPositionArray[i*9+0]*scalingFactor, vertexPositionArray[i*9+1]*scalingFactor, vertexPositionArray[i*9+2]*scalingFactor ),
+                new Ammo.btVector3(vertexPositionArray[i*9+3]*scalingFactor, vertexPositionArray[i*9+4]*scalingFactor, vertexPositionArray[i*9+5]*scalingFactor),
+                new Ammo.btVector3(vertexPositionArray[i*9+6]*scalingFactor, vertexPositionArray[i*9+7]*scalingFactor, vertexPositionArray[i*9+8]*scalingFactor),
+                false
+            );
+    }
+    var shape = new Ammo.btBvhTriangleMeshShape(mesh, true, true);
+    return shape;
+}
+
+
+
 
   const addPug = () => {
 
-
+    let shape;
     loader.load(pugModel, object => {
       object.traverse(child => {
+        if (child.type === 'SkinnedMesh') {
+          shape = createTriangleShapeByBufferGeometry(child.geometry,1);
+        }
         if (child.isMesh) {
           child.material.side = THREE.DoubleSide;
           child.material.shininess = 0.1;
@@ -624,28 +368,17 @@ const MainScene = () => {
       pug.position.y = 10.5;
 
       pug.add(object);
+      pug.add(shape);
       physics.add.existing(pug);
       pug.body.setAngularFactor( 0, 1, 0 );
       pug.body.on.collision((otherObject, event) => {
         //console.log(otherObject.name);
-        if (otherObject.name === 'body_id_19') {
+        if (otherObject.name === 'body_id_19') { // ground
           pug.userData.jumping = false;
           if (pug.userData.move) pug.userData.move.jump = false;
           updateAnimation();
         }
       })
-      //pug.body.setCollisionFlags(2) // make it kinematic
-
-
-      // const pugShape = new CANNON.Box(new CANNON.Vec3(4.5, 3, 1.3))
-      // pugBody = new CANNON.Body({ mass: 80, material: pugMat});
-      // pugBody.addShape(pugShape)
-      // pugBody.angularDamping = 1;
-
-      // pugBody.fixedRotation = true
-      // pugBody.position.set(-20,0,0);
-      // world.addBody(pugBody)
-
 
       cameraPug.position.set(0,5,-50);
 
@@ -684,15 +417,7 @@ const MainScene = () => {
         treat.rotation.y = Math.random() * offset - 1 - offset/2;
         treat.position.z = Math.random() * offset - 1 - offset/2;
         physics.add.existing(treat)
-        //treat.position.normalize();
-        //treat.position.multiplyScalar( 10 );
-        // const treatShape = new CANNON.Box(new CANNON.Vec3(.5, .5, .5))
-        // treat.body = new CANNON.Body({ mass: 10, material: objectMat });
-        // treat.body.addShape(treatShape)
-        // treat.body.position.x = treat.position.x
-        // treat.body.position.y = treat.position.y
-        // treat.body.position.z = treat.position.z
-        // world.addBody(treat.body);
+
         treats.push(treat);
       }
 
@@ -700,7 +425,7 @@ const MainScene = () => {
 
   }
 
-  setupInit();
+  init();
   bindEvents();
   addBushes();
   addFloor();
@@ -718,25 +443,17 @@ const MainScene = () => {
       mixers[i].update(dt);
     }
 
-
-    // treats.forEach(treat => {
-    //   treat.position.set(treat.body.position.x, treat.body.position.y, treat.body.position.z);
-    //   treat.quaternion.set(treat.body.quaternion.x, treat.body.quaternion.y, treat.body.quaternion.z, treat.body.quaternion.w);
-    // })
-
     if (pug) {
-      console.log(pug.position.y);
-      
+
       if(pug.userData.move){
         if (pug.userData.move.forward > 0 && pug.userData.move.speed < 60) pug.userData.move.speed += 1;
 
 
         if (pug.userData.move.jump && !pug.userData.jumping) {
-//            console.log('jump')
           pug.userData.move.jump = false;
           pug.userData.jumping = true;
           pug.body.applyForceY(6);
-          
+
 
         }
 
@@ -769,8 +486,6 @@ const MainScene = () => {
     physics.update(dt * 1000)
     physics.updateDebugger()
 
-    //world.step(dt);
-    //cannonDebugRenderer.update();      // Update the debug renderer
     renderer.render(scene, cameraPug);
     window.requestAnimationFrame(onAnimationFrameHandler);
   }
